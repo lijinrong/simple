ebe0547afb3960c0bfe15a214ab5d3d1ffcbb938
JRLi <lijr22@chinatelecom.cn>
2026-01-07 16:36:07 +0800

    feat: 加编译参数

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 rmdir /q /s build
 mkdir build && cd build
-cmake .. -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=release
+cmake .. -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=release -DCMAKE_C_FLAGS="-static -static-libgcc" -DCMAKE_CXX_FLAGS="-static -static-libgcc -static-libstdc++" -DCMAKE_SHARED_LINKER_FLAGS="-static -static-libgcc -static-libstdc++" 
 make && make install
